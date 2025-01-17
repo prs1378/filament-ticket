7d730b90c6191ab44faaed49ac458680635d6adb
Parsa Parasteh <parsaparasteh@gmail.com>
2025-01-18 00:09:30 +0330

    First Add

---
+++ src/FilamentTicketsServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace YourVendor\FilamentTickets;
+
+use Filament\Support\Assets\Asset;
+use Filament\Support\Facades\FilamentAsset;
+use Filament\Support\Facades\FilamentIcon;
+use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+use YourVendor\FilamentTickets\Livewire\TicketChat;
+use YourVendor\FilamentTickets\Resources\TicketResource;
+
+class FilamentTicketsServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-tickets');
+
+        $this->publishes([
+            __DIR__ . '/../config/filament-tickets.php' => config_path('filament-tickets.php'),
+        ], 'filament-tickets-config');
+
+        $this->publishes([
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
+        ], 'filament-tickets-migrations');
+
+        Livewire::component('ticket-chat', TicketChat::class);
+    }
+
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/filament-tickets.php',
+            'filament-tickets'
+        );
+    }
+} 
